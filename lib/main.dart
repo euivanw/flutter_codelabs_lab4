da625b8653bfb02cf46cd63da69f955eb6088fb3
Ivan Wilhelm <ivan.whm@icloud.com>
2020-05-27 21:05:56 -0300

    Lab4: As classes Row e Column

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'exemplo_row_e_column.dart';
+
+void main() => runApp(MeuApp());
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Laboratório 4',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RowAndColumnWidget(),
+    );
+  }
+}
