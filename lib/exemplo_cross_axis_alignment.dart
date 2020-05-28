441f9bdb7446ba2e9976a8f61366b88a8164ea0b
Ivan Wilhelm <ivan.whm@icloud.com>
2020-05-27 21:51:03 -0300

    Lab4: Modificando o alinhamento cruzado do eixo

---
+++ lib/exemplo_cross_axis_alignment.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CrossAxisAlignmentExemplo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        BlueBox(),
+        BiggerBlueBox(),
+        BlueBox(),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
+
+class BiggerBlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
