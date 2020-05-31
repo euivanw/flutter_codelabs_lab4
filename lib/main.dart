bcda448a252f272387bb68bc5fcb4d328dbd56b8
Ivan Wilhelm <ivan.whm@icloud.com>
2020-05-31 08:46:58 -0300

    Lab4: Mudando a propriedade fit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'exemplo_cross_axis_alignment.dart';
+import 'exemplo_mude_a_propriedade_fit.dart';
 
 void main() => runApp(MeuApp());
 
@@ -13,7 +13,7 @@ class MeuApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CrossAxisAlignmentExemplo(),
+      home: PropriedadeFitExemplo(),
     );
   }
 }
